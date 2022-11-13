9eb5d9fc29a7468d6b15aad9de0571cde32d7a92
mikasaBotz456 <108715025+mikasaBotz456@users.noreply.github.com>
2022-11-13 07:11:03 +0700

    update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://file.io/OFAkOMg60Xj6', m, { packname: "KrizynOfc", author: "@mhdfakri_" })
+conn.sendImageAsSticker(m.chat, 'https://file.io/OFAkOMg60Xj6', m, { packname: "Mikasa", author: "@arrhmdfauzan_" })
 }
 
 handler.customPrefix = /^(ok)$/i
