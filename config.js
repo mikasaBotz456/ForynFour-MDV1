4075c6ec322238c8ffb1fd12510db8d1a8076dbb
mikasaBotz456 <108715025+mikasaBotz456@users.noreply.github.com>
2022-11-20 13:43:53 +0700

    update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.ppreg = 'https://telegra.ph/file/445721f7cdab701543840.jpg'
 global.fotonya1 = 'https://telegra.ph/file/5fc4b42ae2d4e98998f85.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/29aafbe96c2489300639a.jpg' //ini juga ganti 
 global.ppacc = 'https://telegra.ph/file/a077271ff372545599dbc.jpg'
-global.imgloc = 'https://telegra.ph/file/93f369be4e83c85f70b84.jpg'
+global.imgloc = 'https://telegra.ph/file/e6355ade20840c96814ed.jpg'
 global.media = 'https://telegra.ph/file/f209153a54698c43032f7.jpg'
 global.ppm = 'https://telegra.ph/file/d363501313478008b9007.jpg'  // bagian atas all command
 
